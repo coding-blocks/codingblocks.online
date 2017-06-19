ec0418e69856c2511e29ec76074cc148540af427
abhishek97 <zapper.awake@gmail.com>
2017-06-19 18:26:03 +0530

    use srv4

--- config/environment.js
+++ config/environment.js
@@ -29,7 +29,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.HOST = 'http://29964a64.ngrok.io'
+    ENV.HOST = 'http://srv4.cb.lk:3005'
     ENV['simple-auth'] = {
       crossOriginWhitelist:[ '*' ]
     }
