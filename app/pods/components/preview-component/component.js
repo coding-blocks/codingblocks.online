50cb04cac909f3180ce8367dbba02e0ec101fd9e
Umair Khan <omerjerk@gmail.com>
2017-08-18 17:17:48 +0530

    fix: course buy
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/components/preview-component/component.js
+++ app/pods/components/preview-component/component.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  session: Ember.inject.service()
+  session: Ember.inject.service(),
+  'current-user': Ember.inject.service(),
+  init() {
+    this._super();
+    this.set('user', this.get('current-user').get('user'));
+  }
 });
