3308824291a5e5c7b85c1937718d6a6b904c489e
Umair Khan <omerjerk@gmail.com>
2017-08-21 11:12:59 +0530

    move set-offset service to non-pods
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/services/set-offset.js
+++ app/services/set-offset.js
@@ -4,25 +4,22 @@ export default Ember.Service.extend({
   init() {
     this._super(...arguments);
   },
-
   setNavColor(pathName) {
-   var navbar = Ember.$('.custom-nav');
-   var navLink = Ember.$('.custom-nav-link');
+    var navbar = Ember.$('.custom-nav');
+    var navLink = Ember.$('.custom-nav-link');
 
-    if(pathName !== 'index') {
+    if (pathName !== 'index') {
       // Set White
       navbar.removeClass('nav-transparent')
         .addClass('nav-white');
-        navLink.css('color','black');
+      navLink.css('color','black');
 
-    }
-    else {
+    } else {
       // Set Transparent
       navbar.removeClass('nav-white')
         .addClass('nav-transparent')
-         navLink.css('color','white')
+      navLink.css('color','white')
     }
-
   },
   scrollTop() {
     var doc = document.documentElement;
@@ -31,18 +28,17 @@ export default Ember.Service.extend({
     var navLink = Ember.$('.custom-nav-link');
     var mainSection = Ember.$('.img-main').offset().top;
 
-      if (top >= (mainSection - 54) )  {
-        navbar.removeClass('nav-transparent').addClass('nav-white');
-        navLink.css('color','black')
-      }
-      else {
-        navbar.removeClass('nav-white').addClass('nav-transparent');
-        navLink.css('color','white');
-      }
+    if (top >= (mainSection - 54) )  {
+      navbar.removeClass('nav-transparent').addClass('nav-white');
+      navLink.css('color','black')
+    }
+    else {
+      navbar.removeClass('nav-white').addClass('nav-transparent');
+      navLink.css('color','white');
+    }
 
   },
   scrollHomepage() {
     $(window).scroll(this.scrollTop);
   }
-
 });
