1d4f4abe644444748782c97bdda693fdb504d0ea
Umair Khan <omerjerk@gmail.com>
2017-08-24 15:30:15 +0530

    fetch model
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/classroom/run/attempt/content/document/route.js
+++ app/pods/classroom/run/attempt/content/document/route.js
@@ -6,6 +6,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    console.log(params);
+    return this.store.findRecord('document', params.documentId)
   }
 });
