4e68c3986d3c23403ed599db4a6cc8e7997c0141
Umair Khan <omerjerk@gmail.com>
2017-09-01 11:28:02 +0530

    fix redirection
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/pods/components/razorpay-buy-button/component.js
+++ app/pods/components/razorpay-buy-button/component.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
+import ENV from '../../../config/environment';
 
 export default Ember.Component.extend({
+  redirectUrl: "http://localhost:3000/api/checkout", //default value for dev env
+  init() {
+    this._super();
+    this.set('redirectUrl', ENV.apiEndpoint + '/api/checkout');
+  }
 });
