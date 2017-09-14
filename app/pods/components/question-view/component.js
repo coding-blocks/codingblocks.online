b91295090c2b887db0c2ed9b15a6663a04140126
abhishek97 <zapper.awake@gmail.com>
2017-09-14 17:34:33 +0530

    fix marking questions bug

--- app/pods/components/question-view/component.js
+++ app/pods/components/question-view/component.js
@@ -2,8 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    selectChoice (choice) {
+    selectChoice (choice, index) {
       this.get('question').set('selectedChoice', choice);
+      Ember.$('#answer'+index).prop("checked", true)
     }
   }
 
