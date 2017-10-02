f61f6e4963adedf7310c6f8fe4c681940fe11c4a
abhishek97 <zapper.awake@gmail.com>
2017-10-02 15:58:34 +0530

    fix key permissions

--- deployToProduction.sh
+++ deployToProduction.sh
@@ -2,5 +2,6 @@
 
 rm -rf dist/
 ember build --environment=production
+chmod 600 key.pem
 ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "rm -rf ~/frontends/codingblocks.online/*;"
 scp -o StrictHostKeyChecking=no -i key.pem -r dist/* $USER@$SERVER:~/frontends/codingblocks.online
