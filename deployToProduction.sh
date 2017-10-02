93eed8f62db67fc6116c1f1a2368b85b7d7e392d
abhishek97 <zapper.awake@gmail.com>
2017-10-02 15:35:00 +0530

    bower install

--- deployToProduction.sh
+++ deployToProduction.sh
@@ -2,5 +2,5 @@
 
 rm -rf dist/
 ember build --environment=production
-ssh -i key.pem $USER@$SERVER "rm -rf ~/frontends/codingblocks.online/*;"
-scp -i key.pem -r dist/* $USER@$SERVER:~/frontends/codingblocks.online
+ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "rm -rf ~/frontends/codingblocks.online/*;"
+scp -o StrictHostKeyChecking=no -i key.pem -r dist/* $USER@$SERVER:~/frontends/codingblocks.online
