5893183255e26ba1c6c87607b2433e8f5f34fd18
abhishek97 <zapper.awake@gmail.com>
2017-10-12 15:24:04 +0530

    rewrite video player + add shortcuts

--- app/pods/components/video-player/component.js
+++ app/pods/components/video-player/component.js
@@ -1,16 +1,26 @@
-import Component from '@ember/component';
-import { action } from 'ember-decorators/object'
+import Ember from 'ember';
+import KeyboardShortcuts from 'ember-keyboard-shortcuts/mixins/component';
 
-export default class VideoPlayer extends Component {
-  playbackRate = 1
-  toggle = true
-  hls 
-  config
-  playerElement
+export default Ember.Component.extend(KeyboardShortcuts, {
+
+  keyboardShortcuts: {
+    space: 'toggleVideoPlayback',
+    left: 'seekBack',
+    right: 'seekForward',
+    up () {
+      this.send("changeSpeed", 0.5)
+    },
+    down () {
+      this.send("changeSpeed", -0.5)
+    }
+  },
 
   didReceiveAttrs () {
     this._super(...arguments)
-    console.log('didReceiveAttrs')
+
+    if (this.get('copySrc') === this.get('src'))
+      return ;
+
     const self = this
     const config = {
       xhrSetup (xhr, url) {
@@ -29,34 +39,72 @@ export default class VideoPlayer extends Component {
       }
     }
     this.set('config', config)
+    this.set('pr', 1)
+    this.set('isPlaying', false)
+
     const hls = new Hls(config)
-    hls.loadSource(this.get('src'));
+    const video = this.get('playerElement')
+    if (!Ember.isNone(video)) {
+      // already have the element
+      hls.loadSource(this.get('src'));
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED, function () {
+        video.play()
+        this.set('isPlaying', true)
+      })
+    }
     this.set('hls', hls)
-  }
+    this.set('copySrc', this.get('src'))
+  },
+
   didInsertElement () {
     this._super(...arguments)
     console.log('didInsertElement')
     const video = this.$('#video')[0]
-    this.set('playerElement', video)
-  }
-  didRender() {
-    this._super(...arguments)
-    console.log('didRender')
-    const video = this.get('playerElement')
     const hls = this.get('hls')
+
+    this.set('playerElement', video)
+
+    hls.loadSource(this.get('src'));
     hls.attachMedia(video);
     hls.on(Hls.Events.MANIFEST_PARSED, function () {
       video.play()
     })
-  }
+  },
 
-  @action
-  changeSpeed(val){
-     const rate = this.get('playbackRate')+val;
-     const video = this.$('#video')[0];
-     if(rate<=3 && rate>=0.5){
-  	    video.playbackRate = rate;
-      this.set('playbackRate',rate);
-     }
+  actions: {
+    changeSpeed(val) {
+     const rate = +this.get('pr') +val;
+     const video = this.get('playerElement')
+     if ( rate >= -0.5 && rate <= 2) {
+        video.playbackRate = +rate;
+        this.set('pr', rate)
+      }  
+   },
+   toggleVideoPlayback () {
+    const video = this.get("playerElement")
+    if (Ember.isNone(video))
+      return ;
+    this.toggleProperty("isPlaying")
+    const isPlaying = this.get("isPlaying")
+    if (isPlaying) {
+      video.play()
+    } else {
+      video.pause()
+    }
+   },
+   seekBack () {
+    const video = this.get("playerElement")
+    if (Ember.isNone(video))
+      return ;
+    video.currentTime -= 5
+   },
+   seekForward () {
+    const video = this.get("playerElement")
+    if (Ember.isNone(video))
+      return ;
+    video.currentTime += 5
+   }
   }
-}
+  
+})
