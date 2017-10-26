ca20af6e6c8b0a99a2e81b55c1784b505bccb8d4
Siddharth Dungarwal <sd5869@gmail.com>
2017-10-26 16:49:52 +0530

    Added 402 checks

--- app/pods/classroom/run/index/route.js
+++ app/pods/classroom/run/index/route.js
@@ -8,17 +8,23 @@ export default Ember.Route.extend({
     controller.set('run', model);
     controller.set('course', model.get('course'));
     let sections = model.get('course.sections');
+	
+    const nonPremiums = sections.filter( section => !section.get('premium') )
 
-    sections.find( section => {
+    if ( nonPremiums.get('length') === 0 )
+	    return console.log('402: Payment Not Found')
+
+    nonPremiums.find( section => {
       const contentToResume = section.get('contents').find(content => {
-        return content.get('progress').content === null
+        return content.get('progress').content === null 
       })
       if (contentToResume)
         return controller.set('contentToResume', contentToResume.id)
     })
 
+    
     if (Ember.isNone(controller.get('contentToResume')))
-      controller.set("contentToResume", sections.objectAt(0).get('contents').objectAt(0).id)
+      controller.set("contentToResume", nonPremiums.objectAt(0).get('contents').objectAt(0).id)
 
   }
 });
