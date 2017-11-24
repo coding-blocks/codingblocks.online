d65464bc7f58b9e2ac6ff74b5aa0fcae4996f6dc
Abhishek Gupta <hi@itsabhishek.me>
2017-11-24 15:45:10 +0530

    refresh token logic

--- app/authenticators/custom.js
+++ app/authenticators/custom.js
@@ -8,10 +8,15 @@ import Base from 'ember-simple-auth/authenticators/base';
 import env from '../config/environment';
 
 export default Base.extend({
+  refreshTokenTimerId: null,
+  refreshToken : null,
   restore(data) {
-      return new Ember.RSVP.Promise(function (resolve, reject) {
-          if (data.jwt) {
-              resolve(data);
+      return new Ember.RSVP.Promise( (resolve, reject) => {
+          if (!Ember.isNone(data.jwt) && !Ember.isNone(data.refresh_token)) {
+            this.scheduleRefreshTokenRequest(env.refreshTokenTimeout)
+            this.refreshToken = data.refresh_token
+            Ember.run.later(this, this.refreshTokenRequest, this.refreshToken, 0)
+            resolve(data);
           } else {
               console.log("Old logging system detected. Logging out.");
               reject();
@@ -20,9 +25,11 @@ export default Base.extend({
   },
   authenticate() {
       var args = [...arguments];
-      return new Ember.RSVP.Promise(function(resolve, reject) {
-          Ember.$.get(env.apiEndpoint + '/oneauth/login?grant_code=' + args[0], function (data) {
-              if (data.jwt !== undefined) {
+      return new Ember.RSVP.Promise( (resolve, reject) => {
+          Ember.$.get(env.apiEndpoint + '/api/jwt/login?grant_code=' + args[0],  (data) => {
+              if (!Ember.isNone(data.jwt) && !Ember.isNone(data.refresh_token) ) {
+                  this.scheduleRefreshTokenRequest(env.refreshTokenTimeout)
+                  this.refreshToken = data.refresh_token
                   resolve(data);
               } else {
                   reject(data);
@@ -30,9 +37,35 @@ export default Base.extend({
           });
       });
   },
+  // Schedules a refresh token request after @time ms
+  scheduleRefreshTokenRequest (wait) {
+    if (!Ember.isNone(this.refreshTokenTimerId))
+      Ember.run.cancel(this.refreshTokenTimerId)
+
+    const id = Ember.run.later(this, this.refreshTokenRequest, this.refreshToken, wait)
+    this.refreshTokenTimerId = id
+  },
+  refreshTokenRequest () {
+    Ember.$.get(env.apiEndpoint + '/api/jwt/refresh?refresh_token=' + this.refreshToken , (data) => {
+      if (!Ember.isNone(data.jwt)) {
+        this.scheduleRefreshTokenRequest(env.refreshTokenTimeout)
+        this.trigger('sessionDataUpdated', {
+          jwt: data.jwt,
+          refresh_token: this.refreshToken
+        })
+      } else {
+        this.trigger('sessionDataInvalidated')
+        window.location.href = "https://account.codingblocks.com/logout"
+        //TODO: logout of oneauth
+      }
+    })
+  },
   invalidate(data) {
       return new Ember.RSVP.Promise(function (resolve, reject) {
-          resolve();
+        window.setTimeout( () => {
+          window.location.href = "https://account.codingblocks.com/logout?redirect=" + env.publicUrl
+        }, 0)
+        resolve();
       });
   }
 });
