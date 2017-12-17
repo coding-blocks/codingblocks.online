5f659b025f8cbc9487d2ded80f2ad785bce3327d
abhishek97 <zapper.awake@gmail.com>
2017-12-17 14:11:08 +0530

    merge master into production 17-Dec

--- app/pods/components/preview-component/component.js
+++ app/pods/components/preview-component/component.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-import config from '../../../config/environment';
 
 export default Ember.Component.extend({
   session: Ember.inject.service(),
@@ -8,13 +7,7 @@ export default Ember.Component.extend({
     this._super();
   },
   actions: {
-    login() {
-      var redirectionPath = window.location.pathname;
-      redirectionPath = redirectionPath.replace(/^\/|\/$/g, '');
-      localStorage.setItem('redirection-path', redirectionPath);
-      window.location = `https://account.codingblocks.com/oauth/authorize?response_type=code&client_id=${config.clientId}&redirect_uri=${config.publicUrl}`
-    },
-    enrollThisCourse(run) {
+    enrollThisCourse (run) {
       this.get('api').request('/run_attempts', {
         method: 'POST',
         data: {
