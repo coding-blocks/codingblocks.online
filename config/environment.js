ac38486c966a09aa72e68289ccaff6f4a862af16
Abhishek Gupta <hi@itsabhishek.me>
2018-01-10 13:30:09 +0530

    fix repeated login

--- config/environment.js
+++ config/environment.js
@@ -41,7 +41,6 @@ module.exports = function(environment) {
     },
     ENV.publicUrl = 'http://localhost:4200';
     ENV.clientId = 7642172843
-    ENV.refreshTokenTimeout = 20000 // 20 sec
     ENV.oneauthURL = 'https://account.codingblocks.com/'
     // ENV.sentry.development = true
   }
@@ -61,7 +60,6 @@ module.exports = function(environment) {
     ENV.publicUrl = 'https://online.codingblocks.com';
     ENV.apiEndpoint = 'https://api-online.cb.lk';
     ENV.clientId = 5633768694
-    ENV.refreshTokenTimeout = 900000 // 15min
     ENV.oneauthURL = 'https://account.codingblocks.com/'
 
   }
@@ -70,7 +68,6 @@ module.exports = function(environment) {
     ENV.publicUrl = 'http://development.staging.codingblocks.online';
     ENV.apiEndpoint = 'https://codingblocks-online-staging.herokuapp.com';
     ENV.clientId = 5633768694
-    ENV.refreshTokenTimeout = 900000 // 15min
     ENV.oneauthURL = 'https://account.codingblocks.com/'
   }
 
