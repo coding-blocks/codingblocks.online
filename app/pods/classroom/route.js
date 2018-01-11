ac8c663731dbca2de90e0b8c831a27199e4debe6
Abhishek Gupta <hi@itsabhishek.me>
2018-01-11 19:08:04 +0530

    make /classroom route authenticated only

--- app/pods/classroom/route.js
+++ app/pods/classroom/route.js
@@ -4,6 +4,9 @@
 'use strict';
 
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+	//TODO: Change this to tell user, he needs to be loggedIn
+	authenticationRoute: 'index'
 });
