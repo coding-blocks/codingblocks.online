718046291f2084afde6fa454c41c7536c61c3be5
Siddharth Dungarwal <sd5869@gmail.com>
2018-01-30 18:52:50 +0530

    Added a safe check for percentage.

--- app/pods/components/material-card-component/component.js
+++ app/pods/components/material-card-component/component.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    runPercent:Ember.computed('run.percentComplete',function(){
+        return isNaN(this.get('run.percentComplete')) ? '0.00' : this.get('run.percentComplete');
+    })
 });
