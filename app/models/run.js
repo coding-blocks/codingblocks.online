618e948fca063c5035889b90b18fa69005b76d24
Prajjwal Singh <sin@prajjwal.com>
2018-04-30 18:27:53 +0530

    Add: run end and start logic

--- app/models/run.js
+++ app/models/run.js
@@ -8,10 +8,19 @@ export default DS.Model.extend({
   name: DS.attr(),
   start: DS.attr(),
   end: DS.attr(),
+  enrollmentStart: DS.attr (),
+  enrollmentEnd: DS.attr (),
   isFree: DS.attr(),
   price: DS.attr(),
   runAttemptId: DS.attr(),
   course: DS.belongsTo('course'),
   user: DS.belongsTo('user'),
-  percentComplete: DS.attr()
+  percentComplete: DS.attr(),
+  isEnded: Ember.computed ('end', function () {
+    const now = Math.floor (((new Date ()).getTime ()) / 1000),
+      endTime = this.get ('end')
+    ;
+
+    return (now >= endTime)
+  })
 })
